9c05a1c3664beb9b2bd8a58e3b91d9ff895fdea7
Pedro S. Pereira <71725215+devpedrospereira@users.noreply.github.com>
2022-06-25 15:37:25 -0300

    method implementation of routing and transfer of properties between components

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,16 +1,67 @@
-import { Player, Youtube, DefaultUi } from '@vime/react'
-import {DiscordLogo, Lightning, FileArrowDown, CaretRight} from 'phosphor-react'
+import { Player, Youtube, DefaultUi } from "@vime/react";
+import {
+    DiscordLogo,
+    Lightning,
+    FileArrowDown,
+    CaretRight,
+} from "phosphor-react";
 
-import '@vime/core/themes/default.css'
+import "@vime/core/themes/default.css";
+import { gql, useQuery } from "@apollo/client";
 
-export function Video() {
+const GET_LESSON_BY_SLUG_QUERY = gql`
+    query GetLessonBySlug($slug: String) {
+        lesson(where: { slug: $slug }) {
+            title
+            videoId
+            description
+            teacher {
+                bio
+                avatarURL
+                name
+            }
+        }
+    }
+`
+
+interface GetLessonBySlugQuery{
+    lesson:{
+        title: string,
+        videoId: string,
+        description: string,
+        teacher:{
+            bio: string,
+            avatarURL: string,
+            name: string
+        }
+    }
+}
+
+interface VideoProps {
+    lessonSlug: string;
+}
+
+export function Video(props: VideoProps) {
+    const {data} = useQuery<GetLessonBySlugQuery>(GET_LESSON_BY_SLUG_QUERY,{
+        variables: {
+            slug: props.lessonSlug,
+        }
+    });
+
+    if (!data) {
+        return(
+            <div className="flex-1">
+                <p>Carregando...</p>
+            </div>
+        )
+    }
+
     return (
         <div className="flex-1">
-            
             <div className="bg-black flex justify-center">
                 <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
                     <Player>
-                        <Youtube videoId='cUT665tW4v8' />
+                        <Youtube videoId="cUT665tW4v8" />
                         <DefaultUi />
                     </Player>
                 </div>
@@ -20,78 +71,89 @@ export function Video() {
                 <div className="flex items-start gap-16">
                     <div className="flex-1">
                         <h1 className="text-2xl font-bold">
-                            Aula 01 - Criando o projeto e realizando o setup
-                            inicial
+                            {data.lesson.title}
                         </h1>
                         <p className="mt-4 text-gray-200 leading-relaxed">
-                            Nessa aula vamos dar início ao projeto criando a
-                            estrutura base da aplicação utilizando ReactJS, Vite
-                            e TailwindCSS. Vamos também realizar o setup do
-                            nosso projeto no GraphCMS criando as entidades da
-                            aplicação e integrando a API GraphQL gerada pela
-                            plataforma no nosso front-end utilizando Apollo
-                            Client.
+                            {data.lesson.description}
                         </p>
-                        <div className='flex items-center gap-4 mt-6 flex items-center'>
-                            <img 
-                                className='h-16 w-16 rounded-full border-2 border-blue-500'
-                                src="https://github.com/devpedrospereira.png" 
-                                alt="avatar GitHub" 
+                        <div className="flex items-center gap-4 mt-6 flex items-center">
+                            <img
+                                className="h-16 w-16 rounded-full border-2 border-blue-500"
+                                src={data.lesson.teacher.avatarURL}
+                                alt="avatar GitHub"
                             />
 
-                        <a href="https://github.com/devpedrospereira" >
-                            <div className='leading-relaxed'>
-                                <strong className='font-bold text-2xl block'>Pedro S. Pereira</strong>
-                                <span className='text-gray-200 text-sm block'>Estudante @Rocketseat</span>
-
-                            </div>
-                        </a>
+                                <div className="leading-relaxed">
+                                    <strong className="font-bold text-2xl block">
+                                        {data.lesson.teacher.name}
+                                    </strong>
+                                    <span className="text-gray-200 text-sm block">
+                                        {data.lesson.teacher.bio}
+                                    </span>
+                                </div>
                         </div>
                     </div>
 
                     <div className="flex flex-col gap-4">
-                        <a href="" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
-                            <DiscordLogo size={24}/>
+                        <a
+                            href=""
+                            className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors"
+                        >
+                            <DiscordLogo size={24} />
                             Comunidade do Discord
                         </a>
-                        <a href="" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
-                            <Lightning size={24}/>
+                        <a
+                            href=""
+                            className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors"
+                        >
+                            <Lightning size={24} />
                             Acesse o desafio
                         </a>
                     </div>
                 </div>
 
-                <div className='gap-8 mt-20 grid grid-cols-2'>
-                    <a href="" className='bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors'>
-                        <div className='bg-green-700 h-full p-6 flex items-center'>
+                <div className="gap-8 mt-20 grid grid-cols-2">
+                    <a
+                        href=""
+                        className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+                    >
+                        <div className="bg-green-700 h-full p-6 flex items-center">
                             <FileArrowDown size={48} />
                         </div>
-                        <div className='py-6 leading-relaxed'>
-                            <strong className='text-2xl'>Material Complementar</strong>
-                            <p className='text-sm text-gray-200 mt-2'>
-                                Acesse o material complementar para acelerar o seu desenvolvimento
+                        <div className="py-6 leading-relaxed">
+                            <strong className="text-2xl">
+                                Material Complementar
+                            </strong>
+                            <p className="text-sm text-gray-200 mt-2">
+                                Acesse o material complementar para acelerar o
+                                seu desenvolvimento
                             </p>
                         </div>
-                        <div className='h-full p-6 flex items-center'>
-                            <CaretRight size={24}/>
+                        <div className="h-full p-6 flex items-center">
+                            <CaretRight size={24} />
                         </div>
                     </a>
 
-                    <a href="" className='bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors'>
-                        <div className='bg-green-700 h-full p-6 flex items-center'>
+                    <a
+                        href=""
+                        className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+                    >
+                        <div className="bg-green-700 h-full p-6 flex items-center">
                             <FileArrowDown size={48} />
                         </div>
-                        <div className='py-6 leading-relaxed'>
-                            <strong className='text-2xl'>Wallpapers exclusivos</strong>
-                            <p className='text-sm text-gray-200 mt-2'>
-                                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
+                        <div className="py-6 leading-relaxed">
+                            <strong className="text-2xl">
+                                Wallpapers exclusivos
+                            </strong>
+                            <p className="text-sm text-gray-200 mt-2">
+                                Baixe wallpapers exclusivos do Ignite Lab e
+                                personalize a sua máquina
                             </p>
                         </div>
-                        <div className='h-full p-6 flex items-center'>
-                            <CaretRight size={24}/>
+                        <div className="h-full p-6 flex items-center">
+                            <CaretRight size={24} />
                         </div>
                     </a>
-
                 </div>
             </div>
         </div>
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,17 +1,21 @@
 import { Header } from "../components/Header";
+import {useParams} from 'react-router-dom'
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
-
 export function Event() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header/>
-      <main className="flex flex-1">
-        <Video/>
-        <Sidebar/>
-      </main>
-    </div>
-  );
-}
+    const {slug} = useParams<{slug: string}>();
 
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex flex-1">
+                {slug 
+                ? <Video lessonSlug={slug} /> 
+                : <div className="flex-1"/>
+                }
+                <Sidebar />
+            </main>
+        </div>
+    );
+}
