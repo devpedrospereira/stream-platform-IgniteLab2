e8340196f2c9182db6226d2d8f527caf08285b14
Pedro S. Pereira <71725215+devpedrospereira@users.noreply.github.com>
2022-06-25 12:11:58 -0300

    implementation video of youtube, using Vime-react

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,11 +1,19 @@
+import { Player, Youtube, DefaultUi } from '@vime/react'
 import {DiscordLogo, Lightning, FileArrowDown, CaretRight} from 'phosphor-react'
+
+import '@vime/core/themes/default.css'
 
 export function Video() {
     return (
         <div className="flex-1">
             
             <div className="bg-black flex justify-center">
-                <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+                <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+                    <Player>
+                        <Youtube videoId='cUT665tW4v8' />
+                        <DefaultUi />
+                    </Player>
+                </div>
             </div>
 
             <div className="p-8 max-w-[1100px] mx-auto">
