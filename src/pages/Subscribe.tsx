fd6b8ba26675d6f55f41c72b51999d245d5aaf6d
Pedro S. Pereira <71725215+devpedrospereira@users.noreply.github.com>
2022-06-25 17:02:01 -0300

    function Subscribe

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,17 +1,55 @@
+import { gql, useMutation } from "@apollo/client";
+import { useState, FormEvent } from "react";
+import {useNavigate} from 'react-router-dom'
 import { Logo } from "../components/Logo";
 
-export function Subscribe(){
-    return(
+const CREATE_SUBSCRIBER_MUTATION = gql`
+    mutation CreateSubscriber($name: String!, $email: String!) {
+        createSubscriber(data: { name: $name, email: $email }) {
+            id
+        }
+    }
+`;
+
+export function Subscribe() {
+
+    const navigate = useNavigate();
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+
+    const [createSubscriber, {loading}] = useMutation(CREATE_SUBSCRIBER_MUTATION)
+
+    async function handleSubscribe(event: FormEvent) {
+        event?.preventDefault();
+
+        await createSubscriber({
+            variables:{
+                name,
+                email,
+            }
+        })
+        navigate('/event')
+    }
+
+    return (
         <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
             <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
                 <div className="max-w-[640px]">
-                    <Logo/>
+                    <Logo />
 
                     <h1 className="mt-8 text-[2.5rem] leading-tight">
-                        Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong>
+                        Construa uma{" "}
+                        <strong className="text-blue-500">
+                            aplicação completa
+                        </strong>
+                        , do zero, com{" "}
+                        <strong className="text-blue-500">React</strong>
                     </h1>
                     <p className="mt-4 text-gray-200 leading-relaxed">
-                    Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
+                        Em apenas uma semana você vai dominar na prática uma das
+                        tecnologias mais utilizadas e com alta demanda para
+                        acessar as melhores oportunidades do mercado.
                     </p>
                 </div>
 
@@ -20,24 +58,29 @@ export function Subscribe(){
                         Inscreva-se gratuitamente
                     </strong>
 
-                    <form action="" className="flex flex-col gap-2 w-full">
-                        <input 
-                        type="text" 
-                        className="bg-gray-900 rounded px-5 h-14"
-                        placeholder="Seu nome completo"
+                    <form
+                        onSubmit={handleSubscribe}
+                        className="flex flex-col gap-2 w-full"
+                    >
+                        <input
+                            type="text"
+                            className="bg-gray-900 rounded px-5 h-14"
+                            placeholder="Seu nome completo"
+                            onChange={(event) => setName(event.target.value)}
                         />
-                        <input 
-                        type="email" 
-                        className="bg-gray-900 rounded px-5 h-14"
-                        placeholder="Digite seu email"
+                        <input
+                            type="email"
+                            className="bg-gray-900 rounded px-5 h-14"
+                            placeholder="Digite seu email"
+                            onChange={(event) => setEmail(event.target.value)}
                         />
 
                         <button
-                        type="submit"
-                        className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors"
+                            type="submit"
+                            disabled={loading}
+                            className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
                         >
                             Garantir minha vagas
-
                         </button>
                     </form>
                 </div>
@@ -45,6 +88,5 @@ export function Subscribe(){
 
             <img src="/src/assets/code-mokap.png" className="mt-10" alt="" />
         </div>
-    )
+    );
 }
-
